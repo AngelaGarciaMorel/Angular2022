88408cc445dbfe6a63a8ed4e184840d9b5c58b7f
Angela Garcia <a.garcia.morel@gmail.com>
2022-03-29 20:14:35 -0300

    Modificación para agregar pipe

--- ListaGarciaMorel/src/app/components/content-area/content-area.component.ts
+++ ListaGarciaMorel/src/app/components/content-area/content-area.component.ts
@@ -6,7 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content-area.component.css']
 })
 export class ContentAreaComponent implements OnInit {
-  listaAlumnos : String[] = ['Angie Garcia','Mariela Segado','Juan Perez','Pedro Martinez','Raul Portal','Eva Green'];
+  //listaAlumnos : String[] = ['Angie Garcia','Mariela Segado','Juan Perez','Pedro Martinez','Raul Portal','Eva Green'];
+  //listaFechaInscrip : Date[] = [new Date('01/03/2022'),new Date('02/03/2022'),new Date('03/03/2022'),new Date('04/03/2022'),new Date('05/03/2022'),new Date('06/03/2022')];
+  
+  listaAlumnos : any = [
+    {nombre:'Angie Garcia',   fecInscrip : new Date('01/03/2022')},
+    {nombre:'Mariela Segado', fecInscrip : new Date('02/02/2022')},
+    {nombre:'Juan Perez',     fecInscrip : new Date('03/01/2022')},
+    {nombre:'Pedro Martinez', fecInscrip : new Date('04/03/2022')},
+    {nombre:'Raul Portal',    fecInscrip : new Date('05/03/2022')},
+    {nombre:'Eva Green',      fecInscrip : new Date('06/03/2022')}
+  ];
+  
   txtStyle : string = "";
   constructor() { }
 
